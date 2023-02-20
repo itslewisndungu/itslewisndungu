e9dbbefa69f816c2e5947c9fa615fdd6ecf2059d
itslewisndungu <itslewisndungu@gmail.com>
2023-02-20 16:56:45 +0300

    feat(analytics): Add `vercel/analytics`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.scss';
 	import '../app.postcss';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	export const csr = false;
 </script>
